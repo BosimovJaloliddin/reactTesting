b02ce4157df84a8b8ad3045f72fbe3d81ae3ad70
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-06-05 19:16:02 +0500

    multi state change

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -6,6 +6,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       count: 0,
+      login: "",
+      surname: "",
     };
   }
   render() {
@@ -18,11 +20,30 @@ class App extends React.Component {
       console.log(this.state.count);
     };
 
+    const onChange = ({ target: { value, name } }) => {
+      this.setState({ name: (this.state[name] = value) });
+    };
+
     return (
       <React.Fragment>
         <div>Count:{this.state.count}</div>
         <button onClick={onPlus}>+</button>
         <button onClick={onMinus}>-</button>
+        <hr />
+        <h1>Name:{this.state.login}</h1>
+        <h1>Surname:{this.state.surname}</h1>
+        <input
+          name="login"
+          onChange={onChange}
+          type="text"
+          placeholder="login"
+        />
+        <input
+          name="surname"
+          onChange={onChange}
+          type="text"
+          placeholder="surname"
+        />
         {users.map((v) => (
           <h1>
             {v.id}-{v.name}={this.props.data}
