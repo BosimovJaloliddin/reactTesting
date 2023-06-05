5c42d9c83849065cadf2d9daeb1826104300ade3
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-06-05 18:30:55 +0500

    state counter

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-const App = () => {
-  return <div>App</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  render() {
+    const onPlus = () => {
+      this.setState({ count: this.state.count + 1 });
+      console.log(this.state.count);
+    };
+    const onMinus = () => {
+      this.setState({ count: this.state.count - 1 });
+      console.log(this.state.count);
+    };
+
+    return (
+      <React.Fragment>
+        <div>Count:{this.state.count}</div>
+        <button onClick={onPlus}>+</button>
+        <button onClick={onMinus}>-</button>
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
