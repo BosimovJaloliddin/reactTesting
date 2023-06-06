e54dab09081db5a961ede899c59399d12b101904
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-06-06 10:58:14 +0500

    colorReducer

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,29 +1,49 @@
-import React, { Component } from "react";
-import { ThemeProvider, createGlobalStyle } from "styled-components";
+import React, { useReducer } from "react";
 
-const GlobalStyle = createGlobalStyle`
-  body{
-    background: ${(props) => props.theme.bg};
-    color: ${(props) => props.theme.cl};
-  }
-`;
+const App = () => {
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "black":
+        return (state = action.type);
+      case "yellow":
+        return (state = action.type);
+      case "green":
+        return (state = action.type);
+      default:
+        return "red";
+    }
+  }, "white");
+  return (
+    <div>
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          border: "1px solid black",
+          margin: "2rem",
+          background: `${state}`,
+        }}
+      ></div>
+      <button
+        onClick={() => dispatch({ type: "black" })}
+        style={{ marginLeft: "1rem", padding: "20px" }}
+      >
+        black
+      </button>
+      <button
+        onClick={() => dispatch({ type: "yellow" })}
+        style={{ marginLeft: "1rem", padding: "20px" }}
+      >
+        yellow
+      </button>
+      <button
+        onClick={() => dispatch({ type: "green" })}
+        style={{ marginLeft: "1rem", padding: "20px" }}
+      >
+        green
+      </button>
+    </div>
+  );
+};
 
-export default class App extends Component {
-  state = {
-    light: false,
-  };
-  render() {
-    const them = {
-      bg: this.state.light ? "white" : "black",
-      cl: this.state.light ? "black" : "white",
-    };
-    return (
-      <ThemeProvider theme={them}>
-        <GlobalStyle />
-        <button onClick={() => this.setState({ light: !this.state.light })}>
-          click
-        </button>
-      </ThemeProvider>
-    );
-  }
-}
+export default App;
