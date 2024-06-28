34262d81cf5b3267edf51276b83b2dcc6ab71b1d
Jaloliddin Bosimov <bosimovjaloliddin@gmail.com>
2024-06-28 19:28:14 +0500

    test

--- src/Root/root.jsx
+++ src/Root/root.jsx
@@ -1,15 +1,37 @@
-import React from "react";
-import { pages } from "../utils";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
-const Root = () => {
+const App = () => {
+  const [api, setApi] = useState("");
+  const [url, setUrl] = useState("https://jsonplaceholder.typicode.com/todos");
+
+  const getUrl = ({ target: { value } }) => {
+    console.log(value);
+    setUrl(value);
+  };
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((info) => {
+        setApi(info);
+      });
+  }, [url]);
+
+  console.log(api);
+
   return (
-    <Routes>
-      {pages?.map(({ id, path, element }) => {
-        return <Route key={id} path={path} element={element} />;
-      })}
-    </Routes>
+    <div className="wrap">
+      <div className="downloadurl">
+        <input
+          onChange={getUrl}
+          type="text"
+          className="urlinput"
+          placeholder="Enter your YouTube URL"
+        />
+        <button className="btn">Download</button>
+      </div>
+    </div>
   );
 };
 
-export default Root;
+export default App;
